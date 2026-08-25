9fc6ddb8ff97a2864fdf39a5f8bfaae078a0ca42
doflavio <doflavio@gmail.com>
2026-08-25 20:19:45 +0000

    traduz rótulo de espaço livre para português e atualiza prompts de quebra-gelo

--- socops/src/main/java/com/socops/data/IcebreakerPrompts.java
+++ socops/src/main/java/com/socops/data/IcebreakerPrompts.java
@@ -9,33 +9,33 @@ import java.util.List;
  */
 public final class IcebreakerPrompts {
 
-    public static final String FREE_CELL_LABEL = "FREE SPACE";
+    public static final String FREE_CELL_LABEL = "ESPAÇO LIVRE";
 
     public static final List<String> ALL_PROMPTS = List.of(
-            "bikes to work",
-            "has lived in another country",
-            "has a pet",
-            "prefers tea over coffee",
-            "plays an instrument",
-            "speaks more than 2 languages",
-            "has run a marathon",
-            "was born in a different state",
-            "has met a celebrity",
-            "can juggle",
-            "has been skydiving",
-            "loves cooking",
-            "has a garden",
-            "has traveled to Asia",
-            "is left-handed",
-            "has a twin",
-            "plays video games",
-            "does yoga",
-            "has a hidden talent",
-            "loves spicy food",
-            "has been on TV",
-            "collects something unique",
-            "has read a book this month",
-            "knows sign language"
+            "já programou em mais de uma linguagem",
+            "tem um atalho de teclado favorito",
+            "já criou um projeto por diversão",
+            "prefere teclado mecânico",
+            "já participou de um hackathon",
+            "tem um aplicativo indispensável",
+            "já corrigiu um bug de madrugada",
+            "usa tema escuro no editor",
+            "já montou ou atualizou um computador",
+            "tem uma playlist para trabalhar",
+            "já ensinou alguém a usar uma tecnologia",
+            "sabe explicar uma tecnologia de forma simples",
+            "já automatizou uma tarefa repetitiva",
+            "tem um gadget que não vive sem",
+            "já trabalhou em um projeto open source",
+            "já encontrou uma solução em um fórum",
+            "prefere linha de comando a interface gráfica",
+            "já perdeu horas por causa de um detalhe no código",
+            "tem uma extensão favorita no navegador",
+            "já fez um curso de tecnologia por conta própria",
+            "conhece um easter egg de algum software",
+            "já apresentou uma ideia usando tecnologia",
+            "pode mostrar um truque digital em cinco segundos",
+            "aceita jogar pedra, papel e tesoura"
     );
 
     private IcebreakerPrompts() {
